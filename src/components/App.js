d67e03aa4c08ecf2e0bb9013c1c8985a7fab56df
TNsaturday <tnsaturday@gmail.com>
2021-12-11 17:19:51 +0300

    Starter project with App and Article components and fixtures being passed as props

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import Article from "./Article";
+import articles from '../fixtures';
+
+export default function App() {
+  return (
+    <div>
+      <h1>App name</h1>
+      <Article article={articles[0]}/>
+    </div>
+  )
+}
---
+++ src/components/Article.js
@@ -0,0 +1,12 @@
+export default function Article(props) {
+  const {article} = props;
+  const body = <section>{article.text}</section>
+
+  return (
+    <div>
+      <h2>{article.title}</h2>
+      {body}
+      <h3>Creation date: {(new Date(article.date)).toDateString()}</h3>
+    </div>
+  )
+}
