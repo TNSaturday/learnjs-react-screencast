b1bb73abdb2d0225a930951622c81b2087f45205
TNsaturday <tnsaturday@gmail.com>
2021-12-11 20:51:45 +0300

    Same component now written using class component

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,12 +1,31 @@
-export default function Article(props) {
-  const {article} = props;
-  const body = <section>{article.text}</section>
+import { Component } from "react";
 
-  return (
-    <div>
-      <h2>{article.title}</h2>
-      {body}
-      <h3>Creation date: {(new Date(article.date)).toDateString()}</h3>
-    </div>
-  )
+class Article extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOpen: true,
+    }
+  }
+
+  render () {
+    const {article} = this.props;
+    const body = <section>{article.text}</section>
+
+    const handleClick = () => {
+      console.log(this.state);
+    }
+
+    return (
+      <div>
+        <h2>{article.title}</h2>
+        <button onClick={handleClick}>close</button>
+        {body}
+        <h3>Creation date: {(new Date(article.date)).toDateString()}</h3>
+      </div>
+    )
+  }
 }
+
+export default Article;
